80e9733c2fa06c2ca9f8c859bff645c55113d61f
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-09-14 19:59:36 -0700

    created migration files

---
+++ migrations/versions/20230914_195357_create_spots_table.py
@@ -0,0 +1,24 @@
+"""create the spots table
+
+Revision ID: d5e9b5e60518
+Revises: ffdc0a98111c
+Create Date: 2023-09-14 19:53:57.285468
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd5e9b5e60518'
+down_revision = 'ffdc0a98111c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/20230914_195913_create_reviews_table.py
@@ -0,0 +1,24 @@
+"""create reviews table
+
+Revision ID: d34ed201b5d3
+Revises: d5e9b5e60518
+Create Date: 2023-09-14 19:59:13.227848
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd34ed201b5d3'
+down_revision = 'd5e9b5e60518'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
