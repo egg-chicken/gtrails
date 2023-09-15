97d8bd32c70a7efd5be472971343c12a471c1071
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-09-14 21:26:05 -0700

    created spots and reviews models

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(40), nullable=False)
+    lastName = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
@@ -28,6 +30,86 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'firstName': self.firstName,
+            'lastName': self.lastName,
             'username': self.username,
             'email': self.email
         }
+
+    spots_user = db.relationship('Spots', back_populates='user_spot')
+    reviews_user = db.relationship('User', back_populates='user_review')
+
+class Spot(db.Model):
+    __tablename__ = 'spots'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False)
+    state = db.Column(db.String(255), nullable=False)
+    country = db.Column(db.String(255), nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    lng = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    length = db.Column(db.Float, nullable=False)
+    elevGain = db.Column(db.Integer, nullable=False)
+    routeType = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    createdAt = db.Column(db.DateTime, server_default=db.func.now())
+    updatedAt = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'userId': self.userId,
+            'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'country': self.country,
+            'lat': self.lat,
+            'lng': self.lng,
+            'description': self.description,
+            'length': self.length,
+            'elevGain': self.elevGain,
+            'routeType': self.routeType,
+            'image': self.image,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt,
+        }
+
+    user_spot = db.relationship('User', back_populates='spots_user')
+    reviews = db.relationship('Spot', back_populates='spots')
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    spotId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('spots.id')), nullable=False)
+    review = db.Column(db.String(255), nullable=False)
+    stars = db.Column(db.Integer, nullable=False)
+    createdAt = db.Column(db.DateTime, server_default=db.func.now())
+    updatedAt = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'spotId': self.spotId,
+            'review': self.review,
+            'stars': self.stars,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt,
+        }
+
+    user_review = db.relationship('User', back_populates='reviews_user')
+    spots = db.relationship('Spot', back_populates='reviews')
