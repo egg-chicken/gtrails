eef90c71b4e5c10bda61ab304a5ce2d69c23413b
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-09-27 22:59:50 -0700

    added all spot images to landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -25,23 +25,71 @@ const LandingPage = () => {
                     <div className="spot-container-m">
                         {spotsArray.slice(0,4).map((spot) => (
                             <Link key={spot.id} to={`/spots/${spot.id}`} className='spot'>
-                                    <a key={spot.id} href="#" className="spot-card spot">
+                                    <a key={spot.id} href="#" className="spot">
                                     <img src={spot.image} alt='spot' className='spot-images' title={spot.name}/>
-                                    <p className='a-details'>{spot.name}</p>
-                                    <p>{spot.city}, {spot.state}</p>
-                                    <p className='spot-rating'>
-                                        <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
-                                        {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'New'}
-                                    </p>
-                                    <p className='b-details'>Length: {spot.length} mi &#8231; Type: {spot.routeType}</p>
+                                    <div className="spot-details">
+                                        <p className='a-detail'>{spot.name}  </p>
+                                        <p className='spot-rating'>
+                                            <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
+                                            {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'New'}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="b-detail">{spot.city}, {spot.state}</p>
+                                        <p className="b-detail">Length: {spot.length} mi &#8231;  {spot.routeType}</p>
+                                    </div>
                                     </a>
                             </Link>
                         ))}
                     </div>
                 </div>
-                <h2>Cities to explore</h2>
-                <h2>Countries to consider</h2>
-                <h2>Browse by activity</h2>
+                <div className="parks-text">
+                    <p className="parks-text">Locations to explore</p>
+                    <div className="spot-container-m">
+                        {spotsArray.slice(4,8).map((spot) => (
+                            <Link key={spot.id} to={`/spots/${spot.id}`} className='spot'>
+                                    <a key={spot.id} href="#" className="spot">
+                                    <img src={spot.image} alt='spot' className='spot-images' title={spot.name}/>
+                                    <div className="spot-details">
+                                        <p className='a-detail'>{spot.name}  </p>
+                                        <p className='spot-rating'>
+                                            <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
+                                            {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'New'}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="b-detail">{spot.city}, {spot.state}</p>
+                                        <p className="b-detail">Length: {spot.length} mi &#8231;  {spot.routeType}</p>
+                                    </div>
+                                    </a>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <div className="parks-text">
+                    <p className="parks-text">Get away from the city</p>
+                    <div className="spot-container-m">
+                        {spotsArray.slice(8,12).map((spot) => (
+                            <Link key={spot.id} to={`/spots/${spot.id}`} className='spot'>
+                                    <a key={spot.id} href="#" className="spot">
+                                    <img src={spot.image} alt='spot' className='spot-images' title={spot.name}/>
+                                    <div className="spot-details">
+                                        <p className='a-detail'>{spot.name}  </p>
+                                        <p className='spot-rating'>
+                                            <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
+                                            {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'New'}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="b-detail">{spot.city}, {spot.state}</p>
+                                        <p className="b-detail">Length: {spot.length} mi &#8231;  {spot.routeType}</p>
+                                    </div>
+                                    </a>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+
                 <h3>Share your next adventure! by tagging me :) </h3>
 
         </div>
