8ceefe139dbe505b1ec99f35fe25eb78a4721fc7
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-09-30 01:22:03 -0700

    updated manage spots

--- react-app/src/components/ExplorePage/index.js
+++ react-app/src/components/ExplorePage/index.js
@@ -19,16 +19,16 @@ const ExplorePage = () => {
                 <div className='spot-grid'>
                     {spotsArray.map((spot) => (
                         <Link key={spot.id} to={`/spots/${spot.id}`} className='spot'>
-                                    <img src={spot.image} alt='spot prev' className='image' title={spot.name}/>
-                                <div className="spot-details">
-                                    <p className='a-detail'>{spot.name}</p>
-                                    <p className='spot-rating'>
-                                        <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
-                                        {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'No Reviews'}
-                                    </p>
-                                </div>
-                                <p className="b-detail">{spot.city}, {spot.state}</p>
-                                <p className='b-detail'>Length: {spot.length} mi &#8231; Type: {spot.routeType}</p>
+                            <img src={spot.image} alt='spot prev' className='image' title={spot.name}/>
+                            <div className="spot-details">
+                                <p className='a-detail'>{spot.name}</p>
+                                <p className='spot-rating'>
+                                    <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
+                                    {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'No Reviews'}
+                                </p>
+                            </div>
+                            <p className="b-detail">{spot.city}, {spot.state}</p>
+                            <p className='b-detail'>Length: {spot.length} mi &#8231; Type: {spot.routeType}</p>
                         </Link>
                     ))}
                 </div>
--- react-app/src/components/Spots/ManageSpots.js
+++ react-app/src/components/Spots/ManageSpots.js
@@ -5,7 +5,7 @@ import DeleteModal from './DeleteSpotModal';
 import OpenModalButton from '../OpenModalButton';
 import * as spotsActions from '../../store/spots';
 import { Link } from 'react-router-dom';
-
+import './css/manage-spot.css';
 
 const ManageSpotsPage = () => {
     const dispatch = useDispatch();
@@ -18,34 +18,47 @@ const ManageSpotsPage = () => {
     }, [dispatch]);
 
     return (
-        <>
+        <div className='manage-locations-container'>
             <h1>Manage Locations</h1>
-            <button onClick={(e) => {
-                e.stopPropagation()
-                history.push('/spots/new')
-                }}>
-                    Add a New Spot
-            </button>
-            {spots && spotsArray.map((spot) => (
-                <div key={spot.id}>
-                    <Link to={`/spots/${spot.id}`}>
-                    <p>{spot.name}</p>
-                    <p>{spot.description}</p>
-                    </Link>
-                    <OpenModalButton
-                        modalComponent={<DeleteModal id={spot.id}/>}
-                        buttonText = 'Delete'
-                    />
-                    <button onClick={(e) => {
-                        e.stopPropagation()
-                        history.push(`/spots/${spot.id}/edit`)
-                        }}>
-                            Edit
-                    </button>
-                </div>
+            <div className='add-box'>
+                <p className='add-box'>Found a location?</p>
+                <button onClick={(e) => {
+                    e.stopPropagation()
+                    history.push('/spots/new')
+                    }}>
+                        Add a New Spot
+                </button>
+            </div>
+            <div className='spot-grid'>
+                {spotsArray.map((spot) => (
+                    <div key={spot.id}>
+                        <Link to={`/spots/${spot.id}`} className='spot'>
+                            <img src={spot.image} alt='spot prev' className='image' title={spot.name}/>
+                            <div className="spot-details">
+                                <p>{spot.name}</p>
+                                <p className='spot-rating'>
+                                    <i className="fa fa-solid fa-star" style={{color:'#2ced39',}}/>
+                                    {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(1)) : 'No Reviews'}
+                                </p>
+                            </div>
+                            <p className="b-detail">{spot.city}, {spot.state}</p>
+                        </Link>
+                        <OpenModalButton
+                            modalComponent={<DeleteModal id={spot.id}/>}
+                            buttonText="Delete"
+                            buttonType="Delete"
+                        />
+                        <button className='edit-button' onClick={(e) => {
+                            e.stopPropagation()
+                            history.push(`/spots/${spot.id}/edit`)
+                            }}>
+                                Edit
+                        </button>
+                    </div>
 
-            ))}
-        </>
+                ))}
+            </div>
+        </div>
     )
 }
 
