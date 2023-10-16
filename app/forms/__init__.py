a6cf1dbd798b5a1965724c7208291f0aceed4939
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-10-15 23:30:34 -0700

    activity form made

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .review_form import ReviewForm
 from .location_form import LocationForm
+from .activity_form import ActivityForm
